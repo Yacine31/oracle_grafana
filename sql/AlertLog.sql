b779852da715ec65d2107879afed8dddb3a79f63
Yacine31 <yacine.oumghar@gmail.com>
2023-11-19 03:42:25 +0100

    Update AlertLog.sql

--- sql/AlertLog.sql
+++ sql/AlertLog.sql
@@ -1,4 +1,4 @@
 select to_char(ORIGINATING_TIMESTAMP,'YYYY-MM-DD HH24:MI:SS'), MESSAGE_TEXT
   from V$DIAG_ALERT_EXT
   where MESSAGE_TEXT like 'ORA-%' AND originating_timestamp >= SYSDATE - 30
-  order by ORIGINATING_TIMESTAMP;
+  order by ORIGINATING_TIMESTAMP
