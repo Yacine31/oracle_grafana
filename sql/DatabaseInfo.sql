daab20c57a1aa957d6f5d62be077890857708259
Yacine31 <yacine.oumghar@gmail.com>
2023-11-21 01:05:19 +0100

    Update DatabaseInfo.sql

--- sql/DatabaseInfo.sql
+++ sql/DatabaseInfo.sql
@@ -2,7 +2,8 @@ select
     DATABASE_ROLE,
     OPEN_MODE,
     LOG_MODE,
-    to_char(startup_time,'DD-MM-YYYY_HH24:MI') startup_time,
+    -- to_char(startup_time,'DD-MM-YYYY_HH24:MI') startup_time,
+    to_char(startup_time,'DD-MM-YYYY HH24:MI') startup_time,
     STATUS,
     LOGINS
 from v$instance, v$database
