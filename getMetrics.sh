ff50a60ce1fbae331b8482cad0f4052b9beb1493
Yacine31 <yacine.oumghar@gmail.com>
2023-11-27 23:41:03 +0100

    Update getMetrics.sh

--- getMetrics.sh
+++ getMetrics.sh
@@ -1,9 +1,9 @@
 for r in $(ps -eaf | grep pmon | egrep -v 'grep|ASM|APX1' | cut -d '_' -f3)
 do
-        export ORAENV_ASK=NO
         export ORACLE_SID=$r
-        . oraenv -s > /dev/null
-        
+        export ORACLE_HOME=$(cat /etc/oratab | grep "^$r:" | cut -d: -f2)
+        export LD_LIBRARY_PATH=$ORACLE_HOME/lib
+
         export script_dir=/home/oracle/oracle_grafana
 
         /usr/bin/python3 ${script_dir}/getMetrics.py --sid $r --verbose --influxdb-host srvorap --influxdb-port 8086 --influxdb-database influx --sql-directory ${script_dir}/sql
