a35a30b36aec98c8466035e6bd5c1d002be29857
Yacine31 <yacine.oumghar@gmail.com>
2023-12-09 05:02:47 +0100

    Update getMetrics_influxv2.sh

--- getMetrics_influxv2.sh
+++ getMetrics_influxv2.sh
@@ -11,6 +11,6 @@ do
 
         export INFLUXDB_TOKEN=KDDLLyCQ7TzdnW0zr1d0g2C3JbwvX1hgsyJb27DGnVwH8TFp9DUfaIqnC29y6HtWQyODajT20uFpO76AEQNyDw==
 
-        /usr/bin/python3 ${script_dir}/getMetrics_influxv2.py --sid $r --verbose --influxdb-host srvorap --influxdb-port 8087 --influxdb-org homelab --influxdb-database influx --sql-directory ${script_dir}/sql
+        /usr/bin/python3 ${script_dir}/getMetrics_influxv2.py --sid $r --verbose --influxdb-host srvorap --influxdb-port 8087 --influxdb-org homelab --sql-directory ${script_dir}/sql
 
 done
