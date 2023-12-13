ea47776fad3a38ad2a66b68244502a3541d2dc86
Yacine31 <yacine.oumghar@gmail.com>
2023-12-13 07:16:40 +0100

    ajout de la base influx axiome.io

--- getMetrics.sh
+++ getMetrics.sh
@@ -19,10 +19,14 @@ do
         /usr/bin/python3 ${SCRIPT_DIR}/getMetrics.py --sid $sid --verbose --influxdb-host ${INFLUX_HOST} --influxdb-port ${INFLUX_PORT} --influxdb-database ${INFLUX_DB}  --sql-directory ${SCRIPT_DIR}/sql
 
         ## envoyer les données vers l'environnement de prod sur la VM grafana
-        INFLUX_HOST=192.168.31.3
-        INFLUX_PORT=8086
-        INFLUX_DB=telegraf
+        # INFLUX_HOST=192.168.31.3
+        # INFLUX_PORT=8086
+        # INFLUX_DB=telegraf
         
+        INFLUX_HOST=51.38.225.236
+        INFLUX_PORT=8086
+        INFLUX_DB=influx
+
         export INFLUXDB_TOKEN=KDDLLyCQ7TzdnW0zr1d0g2C3JbwvX1hgsyJb27DGnVwH8TFp9DUfaIqnC29y6HtWQyODajT20uFpO76AEQNyDw==
 
         /usr/bin/python3 ${SCRIPT_DIR}/getMetrics.py --sid $sid --verbose --influxdb-host ${INFLUX_HOST} --influxdb-port ${INFLUX_PORT} --influxdb-database ${INFLUX_DB}  --sql-directory ${SCRIPT_DIR}/sql
