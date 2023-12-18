eb1f38f4830589b693e95ad2d1181628bd4c4a98
Yacine31 <yacine.oumghar@gmail.com>
2023-12-18 17:37:58 +0100

    Update DatabaseInfo.sql

--- sql/DatabaseInfo.sql
+++ sql/DatabaseInfo.sql
@@ -1,9 +1,21 @@
+-- select 
+--     DATABASE_ROLE,
+--     OPEN_MODE,
+--     LOG_MODE,
+--     -- to_char(startup_time,'DD-MM-YYYY_HH24:MI') startup_time,
+--     to_char(startup_time,'DD-MM-YYYY HH24:MI') startup_time,
+--     STATUS,
+--     LOGINS
+-- from v$instance, v$database
+
 select 
-    DATABASE_ROLE,
-    OPEN_MODE,
-    LOG_MODE,
-    -- to_char(startup_time,'DD-MM-YYYY_HH24:MI') startup_time,
+    i.host_name,
+    i.instance_name,
+    d.DATABASE_ROLE,
+    d.OPEN_MODE,
+    d.LOG_MODE,
     to_char(startup_time,'DD-MM-YYYY HH24:MI') startup_time,
-    STATUS,
-    LOGINS
-from v$instance, v$database
+    i.STATUS,
+    i.version,
+    i.LOGINS
+from v$instance i, v$database d
