2abe9bd4d0710bf860db3825c1ede8992efc4284
Yacine31 <yacine.oumghar@gmail.com>
2023-12-19 10:56:43 +0100

    Update getMetrics.sh

--- getMetrics.sh
+++ getMetrics.sh
@@ -1,4 +1,4 @@
-for sid in $(ps -eaf | grep pmon | egrep -v 'grep|ASM|APX1' | cut -d '_' -f3)
+for sid in $(ps -eaf | grep pmon | egrep -v 'grep|\+ASM' | cut -d '_' -f3)
 do
         export ORACLE_SID=$sid
         export ORACLE_HOME=$(cat /etc/oratab | grep "^$sid:" | cut -d: -f2)
@@ -9,26 +9,17 @@ do
         cd ${SCRIPT_DIR}
         git pull
 
-        ## envoyer les données vers l'environnement de test sur srvorap
-        # INFLUX_HOST=srvorap
-        # INFLUX_PORT=8086
-        # INFLUX_DB=influx
-
         ## envoyer les données vers l'environnement de prod sur la VM grafana
         INFLUX_HOST=192.168.31.3
         INFLUX_PORT=8086
         INFLUX_DB=telegraf
-
-        export INFLUXDB_TOKEN=KDDLLyCQ7TzdnW0zr1d0g2C3JbwvX1hgsyJb27DGnVwH8TFp9DUfaIqnC29y6HtWQyODajT20uFpO76AEQNyDw==
-
+        
         /usr/bin/python3 ${SCRIPT_DIR}/getMetrics.py --sid $sid --verbose --influxdb-host ${INFLUX_HOST} --influxdb-port ${INFLUX_PORT} --influxdb-database ${INFLUX_DB}  --sql-directory ${SCRIPT_DIR}/sql
 
         
         INFLUX_HOST=51.38.225.236
         INFLUX_PORT=8086
         INFLUX_DB=influx
-
-        export INFLUXDB_TOKEN=KDDLLyCQ7TzdnW0zr1d0g2C3JbwvX1hgsyJb27DGnVwH8TFp9DUfaIqnC29y6HtWQyODajT20uFpO76AEQNyDw==
 
         /usr/bin/python3 ${SCRIPT_DIR}/getMetrics.py --sid $sid --verbose --influxdb-host ${INFLUX_HOST} --influxdb-port ${INFLUX_PORT} --influxdb-database ${INFLUX_DB}  --sql-directory ${SCRIPT_DIR}/sql
 
