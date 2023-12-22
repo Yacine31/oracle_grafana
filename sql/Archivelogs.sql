d0cb3425269d060cc26a5f1b63f7fe5e6d3f0276
Yacine31 <yacine.oumghar@gmail.com>
2023-12-22 18:34:30 +0100

    Update Archivelogs.sql
    calcul du nb d'AL par heure

--- sql/Archivelogs.sql
+++ sql/Archivelogs.sql
@@ -1,13 +1,26 @@
-select
-    -- resetlogs_time,
-    dest_id,
-    sequence# as sequence_number,
-    -- cast(cast(first_time as timestamp with time zone) at time zone 'UTC' as DATE) as "FIRST_TIME",
-    -- completion_time,
-    blocks*block_size as bytes
-from 
-    v$archived_log 
+-- select
+--     -- resetlogs_time,
+--     dest_id,
+--     sequence# as sequence_number,
+--     -- cast(cast(first_time as timestamp with time zone) at time zone 'UTC' as DATE) as "FIRST_TIME",
+--     -- completion_time,
+--     blocks*block_size as bytes
+-- from 
+--     v$archived_log 
+-- where 
+--     completion_time>sysdate - interval '1' day 
+-- order by 
+--     completion_time,dest_id
+
+-- on compte le nombre d'archivelog de la dernière heure
+select 
+    to_char(first_time, 'HH24') "Heure",
+    count(1) "Total"
+from v$log_history
 where 
-    completion_time>sysdate - interval '1' day 
-order by 
-    completion_time,dest_id
+    -- l'heure est l'heure actuelle
+    to_char(first_time, 'HH24') = to_char(systimestamp, 'HH24')
+    and
+    -- le jour est aujourd'hui
+    to_char(first_time, 'YYYYMMDD') = to_char(systimestamp, 'YYYYMMDD') 
+group by to_char(first_time, 'HH24')
