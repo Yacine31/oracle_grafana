e43774499772b0c58ba2e86940587a28ef4e0788
Yacine31 <yacine.oumghar@gmail.com>
2023-12-22 04:22:56 +0100

    Update AlertLog_Count.sql

--- sql/AlertLog_Count.sql
+++ sql/AlertLog_Count.sql
@@ -1,6 +1,12 @@
-select to_char(ORIGINATING_TIMESTAMP, 'DD-MM-YYYY') date_jour, count(*) nb_erreurs 
+-- select to_char(ORIGINATING_TIMESTAMP, 'DD-MM-YYYY') date_jour, count(*) nb_erreurs 
+-- FROM X$DBGALERTEXT
+-- WHERE originating_timestamp > systimestamp - 30 AND 
+-- regexp_like(message_text, '(ORA-)')
+-- group by to_char(ORIGINATING_TIMESTAMP, 'DD-MM-YYYY')
+-- order by to_char(ORIGINATING_TIMESTAMP, 'DD-MM-YYYY') asc
+
+-- On compte le nombre d'erreur ORA- pour la journee en cours :
+SELECT count(*) nb_erreurs 
 FROM X$DBGALERTEXT
-WHERE originating_timestamp > systimestamp - 30 AND 
-regexp_like(message_text, '(ORA-)')
-group by to_char(ORIGINATING_TIMESTAMP, 'DD-MM-YYYY')
-order by to_char(ORIGINATING_TIMESTAMP, 'DD-MM-YYYY') asc
+WHERE to_char(originating_timestamp, 'DD-MM-YYYY') = to_char(systimestamp, 'DD-MM-YYYY') 
+AND regexp_like(message_text, '(ORA-)')
